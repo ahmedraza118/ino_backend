cf5e3940734458ffd3be4e03e2c01eb86b237c8b
Ahmed Raza <ahmedrazachrtx@gmail.com>
2023-07-26 02:05:32 +0500

    cors origin change push

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // Import required modules
-require('dotenv').config();
-const config = require('config');
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import cors module
+require("dotenv").config();
+const config = require("config");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import cors module
 
 // Create Express app
 const app = express();
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors({ origin: "*" })); // Enable CORS for all routes
 // Middleware
-app.use(express.json()); 
+app.use(express.json());
 app.use(bodyParser.json());
 // Register routes
 routes(app);
@@ -20,26 +20,27 @@ routes(app);
 // Connect to the database
 const connectToDatabase = async () => {
   try {
-    const dbConfig = config.get('dbConfig');
+    const dbConfig = config.get("dbConfig");
     let dbUrl = `mongodb://${dbConfig.dbHost}:${dbConfig.dbPort}/${dbConfig.dbName}`;
-  
+
     if (process.env.MONGO_URL) {
       dbUrl = process.env.MONGO_URL;
     }
-  
-    await mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Connected to the database');
+
+    await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the database");
   } catch (error) {
-    console.error('Database connection error:', error);
+    console.error("Database connection error:", error);
     throw error;
   }
-  
-  
 };
 
 // Start the server
 const startServer = () => {
-  const serverConfig = config.get('serverConfig');
+  const serverConfig = config.get("serverConfig");
   const port = process.env.PORT || serverConfig.serverPort;
   app.listen(port, () => {
     console.log(`Server is running on ${serverConfig.serverURL}:${port}`);
@@ -52,6 +53,6 @@ const startServer = () => {
     await connectToDatabase();
     startServer();
   } catch (error) {
-    console.error('Error starting the server:', error);
+    console.error("Error starting the server:", error);
   }
 })();
