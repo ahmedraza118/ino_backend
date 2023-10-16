1738f4e64b1fa5e5fad1042c4cdfa5f4a883ca47
ahmedraza118 <ahmedrazachrtx@gmail.com>
2023-10-16 08:03:03 +0500

    http request size increase

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ app.use(logResponse);
 
 app.use(cors({ origin: "*" })); // Enable CORS for all routes
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(bodyParser.json());
 // Register routes
 routes(app);
